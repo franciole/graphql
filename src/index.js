f127c95b59769948222d51649cc5efce2dfbb937
Franciole Bezerra <franciole.bezerra@gmail.com>
2026-02-05 19:35:50 -0300

    object type custom type no graphql

--- src/index.js
+++ src/index.js
@@ -3,32 +3,38 @@ import { ApolloServer, gql } from 'apollo-server';
 const server = new ApolloServer({
   typeDefs: gql`
     type Query {
-      id: ID
-      name: String
-      age: Int
-      average: Float
-      married: Boolean!
-      arraysString: [String]!
-      arraysInt: [Int]!
-      arraysFloat: [Float]!
-      arraysBoolean: [Boolean]!
+      user: User!,
+      users: [User!]!
+    }
+    type User {
+      id: ID!
+      userName: String!
     }
   `,
   resolvers: {
     Query: {
-      id: () => '123',
-      name: () => 'John Doe',
-      age: () => 30,
-      average: () => 85.5,
-      married: () => false,
-      arraysString: () => ['Hello', 'World'],
-      arraysInt: () => [1, 2, 3],
-      arraysFloat: () => [1.5, 2.5, 3.5],
-      arraysBoolean: () => [true, false, true],
+      user: () => {
+        return {
+          id: '1',
+          userName: 'JohnDoe',
+        };
+      },
+      users: () => {
+        return [
+          {
+            id: '1',
+            userName: 'JohnDoe',
+          },
+          {
+            id: '2',
+            userName: 'JaneSmith',
+          },
+        ];
+      },
     },
   },
 });
 
-server.listen().then(({ url }) => {
-  console.log(`Server listen on url ${url}`);
-});
+  server.listen().then(({ url }) => {
+    console.log(`Server listen on url ${url}`);
+  });
